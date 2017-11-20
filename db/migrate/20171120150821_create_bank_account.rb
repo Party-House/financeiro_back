b1b0f918cefe48a2cfa415c15e129841fa0441f8
andrepestana92 <andre.pest92@gmail.com>
2017-11-20 13:32:10 -0200

    Created table for user and bank account

---
+++ db/migrate/20171120150821_create_bank_account.rb
@@ -0,0 +1,16 @@
+class CreateBankAccount < ActiveRecord::Migration[5.1]
+  def up
+    create_table :bank_account do |t|
+      t.string :account
+      t.string :agency
+      t.string :cpf
+      t.string :agency
+      t.integer :user_id, :null=>false, :references => [:users, :id]
+      t.timestamps
+    end
+  end
+
+  def down
+    drop_table :bank_account
+  end
+end
