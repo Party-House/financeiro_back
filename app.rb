9d534247c535e8443d3f985d643121800f08363a
andrepestana92 <andre.pest92@gmail.com>
2017-11-22 23:04:59 -0200

    Initial basic post endpoint

--- app.rb
+++ app.rb
@@ -3,5 +3,17 @@ require 'sinatra/activerecord'
 require './config/environments' #database configuration
 
 get '/' do
-    "Hello, World!"
+  "Hello, World!"
+end
+
+post '/add-purchase' do
+  return_message = {}
+  request.body.rewind
+  jdata = JSON.parse(
+    request.body.read,
+    :symbolize_names => true
+  )
+  return_message[:status] = "success"
+  return_message[:code] = 200
+  return_message.to_json
 end
