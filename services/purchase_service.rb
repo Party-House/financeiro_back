c388442fea38f2c29a32a47c1a4aca80e5b6c186
andrepestana92 <andre.pest92@gmail.com>
2017-11-28 22:33:33 -0200

    Adding logic for exceptions

--- services/purchase_service.rb
+++ services/purchase_service.rb
@@ -5,7 +5,6 @@ require './models/user'
 class PurchaseService
   def addPurchase (params)
     purchase = Purchase.create params[:purchase]
-    puts(params.has_key? :exceptions)
     begin
       if params.has_key? :exceptions
         params[:exceptions].each do | exception |
@@ -41,13 +40,38 @@ class PurchaseService
     result = []
     total_expenses = Purchase.sum(:value)
     users = User.all
+    user_count = users.length
     users.each do | user |
       user_expense = Purchase.where(:user_id => user.id).sum(:value)
       result << {
         :user_name => user.name,
+        :user_id => user.id,
         :user_expense => user_expense,
-        :user_debt => (total_expenses / 4) - user_expense + user.initial_debt
+        :user_debt => (total_expenses / user_count) - user_expense + user.initial_debt
       }
+    end
+    exceptions_query = PurchaseException.all
+    exceptions = exceptions_query.to_a
+    purchase_id = 0
+    exception_users = []
+    while not exceptions.empty?
+      exception = exceptions.pop
+      puts("exception")
+      purchase_id = exception[:purchase_id]
+      while exception[:purchase_id] == purchase_id
+        exception_users << exception[:user_id]
+        if exceptions.empty?
+          break
+        end
+      end
+      exception_count = exception_users.length
+      result.each do | user |
+        if exception_users.include? user[:user_id]
+          user[:user_debt] -= (Purchase.find(purchase_id).value) / user_count
+        else
+          user[:user_debt] +=  (exception_count / user_count * (user_count - exception_count)) * (Purchase.find(purchase_id).value)
+        end
+      end
     end
     result
   end
